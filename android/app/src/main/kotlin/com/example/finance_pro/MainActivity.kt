b1190d542273aeed0e1e9942daca92dc68e10aba
Hensell <hensell04@hotmail.com>
2023-11-15 14:26:56 -0600

    First deploy

--- android/app/src/main/kotlin/com/example/finance_pro/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.finance_pro
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/services/functions/number_utils.dart
+++
@@ -1,8 +0,0 @@
-import 'package:intl/intl.dart';
-
-class NumberUtils {
-  static double formatAndRound(double number, [int decimals = 4]) {
-    NumberFormat formatter = NumberFormat('0.${'0' * decimals}');
-    return double.parse(formatter.format(number));
-  }
-}
